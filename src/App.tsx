44b647ad0c1180ca80d0b56da75d6103a92b6934
YanaG4 <sinxenon4@gmail.com>
2024-04-08 01:32:18 +0300

    Config TS to import images

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import './App.css';
+import Home from './views/Home';
+import Skills from './views/Skills';
+import Resume from './views/Resume';
+import NavBar from './components/NavBar/NavBar';
+import Projects from './views/Projects/Projects';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+      <NavBar />
+      <div className='main-container'>
+        <Home />
+        <Projects />
+        <Skills />
+        <Resume />
+      </div>
+    </>
   );
 }
 
