fe5df6b79ce36ac00f65d9bfcc72ccae3abe7128
YanaG4 <sinxenon4@gmail.com>
2024-04-09 13:14:45 +0300

    Make skills options a responsive masonry

--- src/views/Skills/Skills.tsx
+++ src/views/Skills/Skills.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Skill from './Skill';
 import { skills } from '../../constants/skills';
+import Masonry, {ResponsiveMasonry} from "react-responsive-masonry";
 
 import './Skills.css';
 
@@ -10,13 +11,17 @@ export default function Skills() {
       <h1 className='section-heading'>SKILLS</h1>
       <div className='section-description'><p>My main working tools are TypeScript, JavaScript, React and Redux. But I'm absolute proficient with all technologies and Instruments mentioned in Frontend & Tools columns. For styling I prefer to use CSS and SASS along with Material UI. Though I’m a Frontend Developer, I have a basic knowledge of Backend technologies and have some experience in working with them.</p></div>
       <div className='skills__skills-options'>
-        {skills.map(field => (
-          <Skill
-            name={field.name}
-            description={field.description}
-            image={field.image}
-            />
-        ))}
+      <ResponsiveMasonry columnsCountBreakPoints={{320: 1, 450: 2, 700: 3, 960: 4, 1200: 5}}>
+        <Masonry gutter='8px'>
+          {skills.map(field => (
+            <Skill
+              name={field.name}
+              description={field.description}
+              image={field.image}
+              />
+          ))}
+        </Masonry>
+        </ResponsiveMasonry>
       </div>
     </section>
   )
