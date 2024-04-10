b65a6d87466d36258d299681fd9aea42b0475646
YanaG4 <sinxenon4@gmail.com>
2024-04-10 17:54:13 +0300

    Refactor NavBar component

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -3,14 +3,39 @@ import { Link } from 'react-scroll';
 
 import './NavBar.css';
 
+interface NavItem {
+  to: string;
+  text: string;
+}
+
+const HEADER_HEIGHT = 72;
+
+const navItems: NavItem[] = [
+  { to: 'home', text: 'HOME' },
+  { to: 'responsibilities', text: 'RESPONSIBILITIES' },
+  { to: 'projects', text: 'PROJECTS' },
+  { to: 'skills', text: 'SKILLS' },
+];
 export default function NavBar() {
+
   return (
     <nav className="navbar--top">
       <ul>
-        <li><Link to="home" smooth={true} duration={500} offset={-72} spy={true} className='navbar__link--top' activeClass='navbar__link--active'>HOME</Link></li>
-        <li><Link to="responsibilities" smooth={true} duration={500} offset={-72} spy={true} className='navbar__link--top' activeClass='navbar__link--active'>RESPONSIBILITIES</Link></li>
-        <li><Link to="projects" smooth={true} duration={500} offset={-72} spy={true} className='navbar__link--top' activeClass='navbar__link--active'>PROJECTS</Link></li>
-        <li><Link to="skills" smooth={true} duration={500} offset={-72} spy={true} className='navbar__link--top' activeClass='navbar__link--active'>SKILLS</Link></li>
+      {navItems.map((item) => (
+        <li key={item.to}>
+          <Link
+            to={item.to}
+            smooth={true}
+            duration={500}
+            offset={-HEADER_HEIGHT}
+            spy={true}
+            className='navbar__link--top'
+            activeClass='navbar__link--active'
+          >
+            {item.text}
+          </Link>
+        </li>
+      ))}
       </ul>
   </nav>
   )
