126c2f014175558f9f9b8fbbe1d663daec1fc239
YanaG4 <sinxenon4@gmail.com>
2024-04-10 17:45:58 +0300

    Bug fixes & code style improvements

--- src/views/Projects/Project.tsx
+++ src/views/Projects/Project.tsx
@@ -6,13 +6,13 @@ import './Project.css'
 interface ProjectProps extends ProjectType {
   onClick: () => void;
   active: boolean;
-  order: number;
+  order?: number | '';
 }
 
 export default function Project({ name, description='My another project', image, onClick, active, order }: ProjectProps) {
   return (
-    <li className={`project ${active && "project--active"}`} onClick={onClick} style={{order}}>
-      <div className={`project__overlay ${!image && "project__overlay--active"}`}>
+    <li className={`project ${active ? "project--active" : ""}`} onClick={onClick} style={{order}}>
+      <div className={`project__overlay ${!image ? "project__overlay--active": ""}`}>
         <p className="project__name">{name}</p>
         <p className="project__description">{description}</p>
       </div>
