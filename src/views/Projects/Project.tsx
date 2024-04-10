6ee97231c2bd2613b526db17d08e826eb39db1e4
YanaG4 <sinxenon4@gmail.com>
2024-04-10 12:28:57 +0300

    Make projects' grid responsive

--- src/views/Projects/Project.tsx
+++ src/views/Projects/Project.tsx
@@ -3,11 +3,15 @@ import type { ProjectType } from '../../constants/projects';
 
 import './Project.css'
 
-type ProjectProps = ProjectType;
+interface ProjectProps extends ProjectType {
+  onClick: () => void;
+  active: boolean;
+  order: number;
+}
 
-export default function Project({ name, description='My another project', image="" }: ProjectProps) {
+export default function Project({ name, description='My another project', image, onClick, active, order }: ProjectProps) {
   return (
-    <li className="project">
+    <li className={`project ${active && "project--active"}`} onClick={onClick} style={{order}}>
       <div className={`project__overlay ${!image && "project__overlay--active"}`}>
         <p className="project__name">{name}</p>
         <p className="project__description">{description}</p>
--- src/views/Projects/Projects.tsx
+++ src/views/Projects/Projects.tsx
@@ -1,20 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ProjectType, projects } from '../../constants/projects';
 import Project from './Project';
 
 import './Projects.css'
 
 export default function Projects() {
+  const [activeProject, setActiveProject] = useState({name: projects[0].name, index: 0});
+
+  const getOrder = (index: number, grid: number, ) => {
+    if(grid < 2) return index;
+    const elementPositionInOrder = activeProject.index + 1;
+    if(elementPositionInOrder % grid !== 0) return index;
+    if (index === activeProject.index - 1)
+      return index + 1;
+    if (index === activeProject.index)
+      return index - 1;
+    return index;
+  };
+
   return (
     <section id='projects' className='projects section-container'>
       <h1 className='section-heading'>PROJECTS</h1>
       <div className='projects-gallery'>
-        {projects.map((project: ProjectType) => (
+        {projects.map((project: ProjectType, index) => (
           <Project
             key={project.name}
             name={project.name}
             description={project.description}
             image={project.image}
+            onClick={() => setActiveProject({name: project.name, index})}
+            active={activeProject.name === project.name}
+            order={getOrder(index, 3)}
           />
         ))}
       </div>
