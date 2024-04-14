df213e2dc7aec5eab48964cdaa498fecc83fb3ee
YanaG4 <sinxenon4@gmail.com>
2024-04-14 19:14:40 +0300

    Make gaps unified

--- src/views/Responsibilities/Responsibilities.tsx
+++ src/views/Responsibilities/Responsibilities.tsx
@@ -11,15 +11,19 @@ import './Responsibilities.css'
 export default function Responsibilities() {
   return (
     <section id='responsibilities' className='responsibilities section-container'>
-      <h1 className='section-heading'>RESPONSIBILITIES</h1>
-      <h2>Do you want me to...</h2>
+      <div className='heading-description-container'>
+        <h1 className='section-heading'>RESPONSIBILITIES</h1>
+        <h2>Do you want me to...</h2>
+      </div>
       <div className='project-types'>
         {projects.map(project => (
           <Project key={project.description} description={project.description} image={project.image} name={project.name} />
         ))}
       </div>
-      <h2>No problem! For the chosen task</h2>
-      <h1 className='section-heading'>I ENSURE</h1>
+      <div className='heading-description-container'>       
+        <h2>No problem! For the chosen task</h2>
+        <h1 className='section-heading'>I ENSURE</h1>
+      </div>
       <ResponsiveMasonry columnsCountBreakPoints={{300: 1,800: 2}}>
         <Masonry gutter='24px'> 
           {benefits.map(benefit => (
